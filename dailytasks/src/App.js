974a0ab71869d622307318a9f1d2b7a68d09ca05
lalitharani508 <lalithad260@gmail.com>
2025-02-17 20:45:11 +0530

    files and task

---
+++ dailytasks/src/App.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+
+import { Parent, App1 } from "./pcComponents/task";
+// import App1 from "./pcComponents/task"
+  const App=()=>{
+    let objdata={
+      name:"Ram",
+      age:26,
+      location:"banglore",
+      job:"Tester"
+    };
+  
+    return(
+      <div>
+        <Parent data={objdata}/>
+      </div>
+    )
+  }
+  export  {App};
+
+
+  // Passing an Array of Objects from Parent to Child
+
+const  arr=()=>{
+    const arrobj=[
+    {
+      name:"Ram",
+      age:26,
+      location:"banglore",
+      job:"Tester",
+      salary:600000
+
+    },{
+      name:"Ramya",
+      age:36,
+      location:"hyderabad",
+      job:"Developer",
+      salary:2500000
+
+    },
+    {
+      name:"Lalitha",
+      age:24,
+      location:"banglore",
+      job:"Software Engineer",
+      salary:5000000
+    }
+    ]
+  return(
+    <div>
+      <App1 details={arrobj}/>
+    </div>
+  )
+}
+export default arr;
---
+++ dailytasks/src/pcComponents/task.js
@@ -0,0 +1,43 @@
+
+import React from "react";
+const Parent=({data})=>{
+    console.log(data);
+    
+    return(
+        <div >
+            <h1>child components</h1>
+            <h2>name:{data.name}</h2>
+            <h2>location:{data.location}</h2>
+        </div>
+    )
+}
+export {Parent};
+
+
+// import React from "react";
+const App1=({details})=>{
+    console.log(details);
+    
+    return(
+        <div>
+            <h2>
+            Child Component
+            </h2>
+            {
+                details.map((person,index)=>(
+                    
+                        <div key={index}>
+                            <h2>{person.name}</h2>
+                            <h2>{person.salary}</h2>
+                            <h2>{person.age}</h2>
+                            <h2>{person.location}</h2>
+
+
+                        </div>
+                    
+                ))
+            }
+        </div>
+    )
+}
+export {App1};
